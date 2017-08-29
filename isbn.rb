9582f09bd247db18f5412c128335856133c6e6cf
Ty-Zeta <tyofzeta@gmail.com>
2017-08-29 15:30:01 -0400

    return true on mini-function

--- isbn.rb
+++ isbn.rb
@@ -8,12 +8,14 @@ check4 = "123456789012-3"
 
 check5 = "123456789 0-123"
 
-check1_sum = 1*1, 2*2, 3*3, 4*4, 5*5, 6*6, 7*7, 8*8, 9*9
-p check1_sum
-
-check1_sum_add = 1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81
-p check1_sum_add
-
-check1_div = 285 % 11
-p check1_div
-
+def checking1
+    check1_sum = 1*1, 2*2, 3*3, 4*4, 5*5, 6*6, 7*7, 8*8, 9*9
+    check1_sum = 1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81
+    check1_sum = 285 % 11
+        if check1_sum == 10
+            p "true"
+        else
+            p "false"
+        end
+    end
+checking1
