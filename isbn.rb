88b91b1cd11a28270bf3f184a9abc3eb35ecc83a
Ty-Zeta <tyofzeta@gmail.com>
2017-08-29 15:12:41 -0400

    understanding sumcheck calculation

--- isbn.rb
+++ isbn.rb
@@ -7,3 +7,13 @@ check3 = "123456789-0"
 check4 = "123456789012-3"
 
 check5 = "123456789 0-123"
+
+check1_sum = 1*1, 2*2, 3*3, 4*4, 5*5, 6*6, 7*7, 8*8, 9*9
+p check1_sum
+
+check1_sum_add = 1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81
+p check1_sum_add
+
+check1_div = 285 % 11
+p check1_div
+
