55693a5af8835a2419422b73a706c612febce4f0
Ty-Zeta <tyofzeta@gmail.com>
2017-08-30 10:10:20 -0400

    scrapped everything, starting over

--- isbn.rb
+++ isbn.rb
@@ -1,38 +1,97 @@
-check1 ="1234567890"
-
-check2 = "1234567890123"
-
-check3 = "123456789-0"
-
-check4 = "123456789012-3"
-
-check5 = "123456789 0-123"
-
-def checking1
-    check1_sum = 1*1, 2*2, 3*3, 4*4, 5*5, 6*6, 7*7, 8*8, 9*9
-    check1_sum = 1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81
-    check1_sum = 285 % 11
-        if check1_sum == 10
-            p "true"
-        else
-            p "false"
-        end
-    end
-checking1
-
-def checking2
-    isbn = gets.chomp
-    x = 10
-    isbn = isbn.gsub(/[ -]/, '')
-    isbn = isbn.split(//)
-    checksum = isbn.pop
-    p checksum
-    check = 1 * isbn[0].to_i + 2 * isbn[1].to_i + 3 * isbn[2].to_i + 4 * isbn[3].to_i + 5 * isbn[4].to_i + 6 * isbn[5].to_i + 7 * isbn[6].to_i + 8 * isbn[7].to_i + 9 * isbn[8].to_i
-    sum = check % 11
-        if sum == x
-            p "VALID"
-        else
-            p "INVALID"
-        end
+def empty_array
+    []
 end
-checking2
+
+def isbn_length(isbn)
+    if isbn.length == 10
+        true
+    elsif isbn.length == 13
+        true
+    else
+        false
+    end
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# check1 ="1234567890"
+
+# check2 = "1234567890123"
+
+# check3 = "123456789-0"
+
+# check4 = "123456789012-3"
+
+# check5 = "123456789 0-123"
+
+# # def checking1
+# #     check1_sum = 1*1, 2*2, 3*3, 4*4, 5*5, 6*6, 7*7, 8*8, 9*9
+# #     check1_sum = 1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81
+# #     check1_sum = 285 % 11
+# #         if check1_sum == 10
+# #             p "true"
+# #         else
+# #             p "false"
+# #         end
+# #     end
+# # checking1
+
+# def checking2
+#     x = 10
+#     isbn = isbn.gsub(/[ -abcdefghijklmnopqrstuvwyz]/, '')
+#     isbn = isbn.split(//)
+#     checksum = isbn.pop
+#     p checksum
+#     check = 1 * isbn[0].to_i + 2 * isbn[1].to_i + 3 * isbn[2].to_i + 4 * isbn[3].to_i + 5 * isbn[4].to_i + 6 * isbn[5].to_i + 7 * isbn[6].to_i + 8 * isbn[7].to_i + 9 * isbn[8].to_i
+#     sum = check % 11
+#         if sum == x
+#             p "VALID"
+#         else
+#             p "INVALID"
+#         end
+# end
+
+# def checking3
+#     x = 10
+#     isbn = isbn.gsub(/[ -abcdefghijklmnopqrstuvwyz]/, '')
+#     isbn = isbn.split(//)
+#     checksum = isbn.pop
+#     p checksum
+#     check = 1 * isbn[0].to_i + 3 * isbn[1].to_i + 1 * isbn[2].to_i + 3 * isbn[3].to_i + 1 * isbn[4].to_i + 3 * isbn[5].to_i + 1 * isbn[6].to_i + 3 * isbn[7].to_i + 1 * isbn[8].to_i + 3 * isbn[9] + 1 * isbn[10] + 3 * isbn[11]
+#     sum = check % 11
+#         if sum == x
+#             p "VALID"
+#         else
+#             p "INVALID"
+#         end
+# end
+
+# def verify
+#     isbn = "1234567890"
+#     isbn = isbn.gsub(/[ -abcdefghijklmnopqrstuvwyz]/, '')
+#         if isbn.length == 10
+#             checking2
+#         elsif isbn.length == 13
+#             checking3
+#         end
+# end
+# verify
--- isbn_test.rb
+++ isbn_test.rb
@@ -7,32 +7,56 @@ class TestIsbn < Minitest::Test
         assert_equal(1,1)
     end
 
+    def test_emptyness
+        ea = []
+        assert_equal(ea, empty_array)
+    end
+
     def test_length_10
-        check1 = "1234567890"
-        assert_equal(10, check1.length)
+        assert_equal(true, isbn_length("1234567890"))
     end
 
     def test_length_13
-        check2 = "1234567890123"
-        assert_equal(13, check2.length)
+        assert_equal(true, isbn_length("1234567890123"))
     end
 
-    def test_length_10_v2
-        check3 = "123456789-0"
-        check3 = check3.gsub(/-/, '')
-        assert_equal(10, check3.length)
-    end
 
-    def test_length_13_v2
-        check4 = "123456789012-3"
-        check4 = check4.gsub(/-/, '')
-        assert_equal(13, check4.length)
-    end
-
-    def test_space_dash
-        check5 = "123456789 0-123"
-        check5 = check5.gsub(/[ -]/, '')
-        assert_equal(13, check5.length)
-    end
 
 end
+
+
+
+
+
+
+
+
+#     def test_length_10
+#         check1 = "1234567890"
+#         assert_equal(10, check1.length)
+#     end
+
+#     def test_length_13
+#         check2 = "1234567890123"
+#         assert_equal(13, check2.length)
+#     end
+
+#     def test_length_10_v2
+#         check3 = "123456789-0"
+#         check3 = check3.gsub(/-/, '')
+#         assert_equal(10, check3.length)
+#     end
+
+#     def test_length_13_v2
+#         check4 = "123456789012-3"
+#         check4 = check4.gsub(/-/, '')
+#         assert_equal(13, check4.length)
+#     end
+
+#     def test_space_dash
+#         check5 = "123456789 0-123"
+#         check5 = check5.gsub(/[ -]/, '')
+#         assert_equal(13, check5.length)
+#     end
+
+# end
