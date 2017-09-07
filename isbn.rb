cf95ec77be2becd4bc03ff8505fe5f8aa85db87c
Ty-Zeta <tyofzeta@gmail.com>
2017-09-07 14:34:27 -0400

    code works now

--- isbn.rb
+++ isbn.rb
@@ -22,10 +22,10 @@ def isbn_function(user_given_isbn)
                 answer_array << sum
             end
 
-            sum_answer_array = answer_array.inject(0, :+)
-            mod_answer_array = sum_answer_array % 11
+            sum_answer = answer_array.inject(0, :+)
+            mod_answer = sum_answer % 11
 
-                if mod_answer_array == last_digit.to_i
+                if mod_answer == last_digit.to_i
                     return_variable = true
             
                 else
@@ -40,16 +40,16 @@ def isbn_function(user_given_isbn)
                 if index_position.to_i % 2 == 0
                     sum = value.to_i * 1
                     answer_array << sum
-                
+              
                 else
                     sum = value.to_i * 3
                     answer_array << sum
                 end
             end
 
-            sum_answer_array = answer_array.inject(0, :+)
-            mod_answer_array = (10 - (sum_answer_array % 10)) % 10
-                if mod_answer_array == last_digit.to_i
+            sum_answer = answer_array.inject(0, :+)
+            mod_answer = (10 - (sum_answer % 10)) % 10
+                if mod_answer == last_digit.to_i
                     return_variable = true
                 
                 else
@@ -61,29 +61,29 @@ def isbn_function(user_given_isbn)
         end
 
     elsif no_space_or_dash_isbn.chop !~ /\D/
-        if no_space_or_dash_isbn == 10
+        if no_space_or_dash_isbn.length == 10
             isbn_array = no_space_or_dash_isbn.split('')
             last_digit = isbn_array.pop
-            
-            isbn_array.each_with_index do |value, index|
+     
+            isbn_array.each_with_index do |value, index_position|
             sum = ((index_position.to_i + 1) * value.to_i)
             answer_array << sum
         end
 
-        sum_answer_array = answer_array.inject(0, :+)
-        mod_answer_array = sum_answer_array % 11
+        sum_answer = answer_array.inject(0, :+)
+        mod_answer = sum_answer % 11
 
-        if mod_answer_array <= 9
-            mod_answer_array   
+        if mod_answer <= 9
+            mod_answer   
         
-        elsif mod_answer_array == 10
-            mod_answer_array = "x"
+        elsif mod_answer == 10
+            mod_answer = "x"
         
         else
             return_variable = false
         end
 
-        if mod_answer_array == last_digit.downcase
+        if mod_answer == last_digit.downcase
             return_variable = true
         
         else
@@ -105,9 +105,9 @@ def isbn_function(user_given_isbn)
             end
         end
 
-        sum_answer_array = answer_array.inject(0, :+)
-        mod_answer_array = (10 - (sum % 10)) % 10
-            if mod_answer_array == last_digit.to_i
+        sum_answer = answer_array.inject(0, :+)
+        mod_answer = (10 - (sum % 10)) % 10
+            if mod_answer == last_digit.to_i
                 return_variable = true
             
             else
